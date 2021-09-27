9ca3657759dd352b5e1e34c4e09c661b4ea93bf9
009Schmitt <34982675+009Schmitt@users.noreply.github.com>
2021-09-27 09:35:36 -0300

    InitialCommit

---
+++ ApiManha/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ApiManha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEnvia = new System.Windows.Forms.Button();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.btnBusca = new System.Windows.Forms.Button();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMail.Location = new System.Drawing.Point(12, 246);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(776, 98);
+            this.txtMail.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(329, 350);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(234, 88);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEnvia
+            // 
+            this.btnEnvia.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnvia.Location = new System.Drawing.Point(569, 350);
+            this.btnEnvia.Name = "btnEnvia";
+            this.btnEnvia.Size = new System.Drawing.Size(219, 88);
+            this.btnEnvia.TabIndex = 2;
+            this.btnEnvia.Text = "ENVIA";
+            this.btnEnvia.UseVisualStyleBackColor = true;
+            this.btnEnvia.Click += new System.EventHandler(this.btnEnvia_Click);
+            // 
+            // txtCep
+            // 
+            this.txtCep.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCep.Location = new System.Drawing.Point(12, 12);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(289, 62);
+            this.txtCep.TabIndex = 3;
+            // 
+            // btnBusca
+            // 
+            this.btnBusca.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBusca.Location = new System.Drawing.Point(12, 80);
+            this.btnBusca.Name = "btnBusca";
+            this.btnBusca.Size = new System.Drawing.Size(289, 62);
+            this.btnBusca.TabIndex = 4;
+            this.btnBusca.Text = "Busca";
+            this.btnBusca.UseVisualStyleBackColor = true;
+            this.btnBusca.Click += new System.EventHandler(this.btnBusca_Click);
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.Location = new System.Drawing.Point(318, 12);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(173, 63);
+            this.lblCidade.TabIndex = 5;
+            this.lblCidade.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.btnBusca);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.btnEnvia);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtMail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEnvia;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Button btnBusca;
+        private System.Windows.Forms.Label lblCidade;
+    }
+}
+
---
+++ ApiManha/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApiManha
+{
+    public partial class Form1 : Form
+    {
+        private List<string> Emails = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //// Cria o email
+            //MailMessage eMail = new MailMessage();
+
+            //// define os endereços
+            //eMail.From = new MailAddress("testeentra21@gmail.com");
+            //eMail.To.Add("testeentra21@gmail.com");
+
+            //// define a mensagem
+            //eMail.Subject = "Titulo";
+            //eMail.Body = "Mensgem";
+
+            //// Enviar o Email
+            //SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            //smtp.EnableSsl = true;
+            //NetworkCredential cred = new NetworkCredential("testeentra21@gmail.com", "@Senac2021");
+            //smtp.Credentials = cred;
+            //smtp.Send(eMail);
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Emails.Add(txtMail.Text);
+            txtMail.Clear();
+        }
+
+        private void btnEnvia_Click(object sender, EventArgs e)
+        {
+            // Cria o email
+            MailMessage eMail = new MailMessage();
+
+            // define os endereços
+            eMail.From = new MailAddress("testeentra21@gmail.com");
+
+            foreach (var item in Emails)
+            {
+                eMail.To.Add(item);
+            }
+
+            // define a mensagem
+            eMail.Subject = "Titulo";
+            eMail.Body = "Mensgem";
+
+            // Enviar o Email
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            NetworkCredential cred = new NetworkCredential("testeentra21@gmail.com", "@Senac2021");
+            smtp.Credentials = cred;
+            smtp.Send(eMail);
+        }
+
+        private void btnBusca_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
